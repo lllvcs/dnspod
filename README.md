7ace95430ff68ffb2706037c029c3cdefc8859c6
lvcs <22395524+lllvcs@users.noreply.github.com>
2020-01-12 13:59:27 +0800

    New

--- README.md
+++ README.md
@@ -1,49 +1,18 @@
-# ArDNSPod
+# DNSPOD Bash Shell
 
-基于DNSPod用户API实现的纯Shell动态域名客户端，适配网卡地址。
+基于DNSPod用户API实现的纯Shell动态域名客户端，适配外网地址。
 
 # Usage
 
-复制`dns.conf.example`到同一目录下的`dns.conf`并根据你的配置修改即可。
+下载dnspod.sh文件，并根据你的配置修改即可。
 
-执行时直接运行`ddnspod.sh`，支持cron任务。
+执行时直接运行`dnspod.sh`，支持cron任务。
 
 配置文件格式：
-```
-# 安全起见，不推荐使用密码认证
-# arMail="Your_E-mail_Address"
-# arPass="123"
 
-# 推荐使用Token认证
 # 按`TokenID,Token`格式填写
 arToken="12345,7676f344eaeaea9074c123451234512d"
 
-# 每行一个域名
-arDdnsCheck "test.org" "subdomain"
-```
-
-# 最近更新
-
-2015/2/24
-- 增加token鉴权方式 (by wbchn)
-
-2015/7/7
-- 使用D+服务获取域名解析
-
-2016/2/25
-- 增加配置文件，分离脚本与配置，适配内网。
-- 加入Mac支持
-- sed脚本POSIX化，可跨平台
-
-2016/3/23
-- 进一步POSIX化，支持Mac和大部分Linux发行版
-- 更改配置文件格式
-
-2018/4/29
-- 改为从外网(ipip.net等)获取ip地址
-
-# Credit
-
-Original: anrip
-
-This version maintained by LLLVCS
+# 填写域名信息
+domain="Your_Domain_Name"
+subdomain="Your_Sub_Domain_Name"
---
+++ dnspod.sh
@@ -0,0 +1,110 @@
+#!/bin/sh
+# 目前dnspod仅支持密钥登录，申请地址 https://console.dnspod.cn/account/token
+# 请填写Token 域名等信息
+# arToken="你的密钥ID,你的密钥Token"
+# domain="你的主域名"
+# subdomain="你的子域"
+
+arToken="TokenID,Token"
+domain="Your_Domain_Name"
+subdomain="Your_Sub_Domain_Name"
+
+arIpAddress() {
+    curltest=$(which curl)
+    if [ -z "$curltest" ] || [ ! -s "$(which curl)" ]; then
+        curl -L -k -s "https://api.ip.la" | grep -E -o '([0-9]+\.){3}[0-9]+'
+        #curl -k -s "http://members.3322.org/dyndns/getip" | grep -E -o '([0-9]+\.){3}[0-9]+'
+        #curl -k -s ip.6655.com/ip.aspx | grep -E -o '([0-9]+\.){3}[0-9]+'
+        #curl -k -s ip.3322.net | grep -E -o '([0-9]+\.){3}[0-9]+'
+        #curl -k -s ip.sb | grep -E -o '([0-9]+\.){3}[0-9]+'
+    else
+        wget --no-check-certificate --quiet --output-document=- "https://api.ip.la" | grep -E -o '([0-9]+\.){3}[0-9]+'
+        #wget --no-check-certificate --quiet --output-document=- "http://members.3322.org/dyndns/getip" | grep -E -o '([0-9]+\.){3}[0-9]+'
+        #wget --no-check-certificate --quiet --output-document=- "ip.6655.com/ip.aspx" | grep -E -o '([0-9]+\.){3}[0-9]+'
+        #wget --no-check-certificate --quiet --output-document=- "ip.3322.net" | grep -E -o '([0-9]+\.){3}[0-9]+'
+        #wget --no-check-certificate --quiet --output-document=- "ip.sb" | grep -E -o '([0-9]+\.){3}[0-9]+'
+
+    fi
+}
+
+arIpAddress=$(arIpAddress)
+
+arDdnsInfo() {
+    local recordID recordIP
+
+    # Get Record ID
+    recordID=$(arApiPost "Record.List" "domain=$domain&sub_domain=$subdomain")
+    recordID=$(echo $recordID | sed 's/.*\[{"id":"\([0-9]*\)".*/\1/')
+
+    # Last IP
+    recordIP=$(arApiPost "Record.Info" "domain=$domain&record_id=$recordID")
+    recordIP=$(echo $recordIP | sed 's/.*,"value":"\([0-9\.]*\)".*/\1/')
+
+    # Output IP
+    case "$recordIP" in
+    [1-9]*)
+        echo $recordIP
+        ;;
+    *)
+        echo "$recordIP"
+        echo "Get Record Info Failed!"
+        ;;
+    esac
+}
+
+arApiPost() {
+    local inter="https://dnsapi.cn/${1:?'Info.Version'}"
+    local param="login_token=${arToken}&format=json&${2}"
+    curl -s -k -X POST $inter -d "$param"
+}
+
+arDdnsUpdate() {
+    local recordID recordRS recordCD recordIP myIP
+
+    # Get Record ID
+    recordID=$(arApiPost "Record.List" "domain=${domain}&sub_domain=${subdomain}")
+    recordID=$(echo $recordID | sed 's/.*\[{"id":"\([0-9]*\)".*/\1/')
+
+    # Update IP
+    myIP=$(arIpAddress)
+    recordRS=$(arApiPost "Record.Ddns" "domain=${domain}&record_id=${recordID}&sub_domain=${subdomain}&record_type=A&value=${myIP}&record_line=默认")
+    recordCD=$(echo $recordRS | sed 's/.*{"code":"\([0-9]*\)".*/\1/')
+    recordIP=$(echo $recordRS | sed 's/.*,"value":"\([0-9\.]*\)".*/\1/')
+
+    # Output IP
+    if [ "$recordIP" = "$myIP" ]; then
+        if [ "$recordCD" = "1" ]; then
+            echo $recordIP
+        fi
+        # Echo error message
+        echo $recordRS | sed 's/.*,"message":"\([^"]*\)".*/\1/'
+    else
+        echo "Update Failed! Please check your network."
+    fi
+}
+
+hostIP=$(arIpAddress)
+lastIP=$(arDdnsInfo)
+
+echo "Updating Domain: $subdomain.$domain"
+echo "HostIP: ${hostIP}"
+echo "LastIP: ${lastIP}"
+if [ "$lastIP" != "$hostIP" ]; then
+
+    case "$lastIP" in
+    [1-9]*)
+        postRS=$(arDdnsUpdate)
+        echo "postRS: ${postRS}"
+        echo "Domain A record has been updated!"
+        ;;
+    *)
+        echo "Got LastIP Error! Stopped!"
+        ;;
+    esac
+
+elif [ "$lastIP" == "$hostIP" ]; then
+    echo "Last Domain A Record Is The Same as The Current IP!"
+    echo "Skipped!"
+else
+    echo "ERROR!"
+fi
